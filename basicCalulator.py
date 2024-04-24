38feda3ac6f778fc1f0890f21f30fe0f9ad82e27
Electiveclone45 <92637810+Electiveclone45@users.noreply.github.com>
2024-04-24 17:27:11 +1000

    Update basicCalulator.py

--- basicCalulator.py
+++ basicCalulator.py
@@ -1,10 +1,15 @@
+#takes in user nickname.
 nickName = input("Enter your nickname: ")
 
+#Collects 2 numbers to conduct calculations on.
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter another number: "))
 
+#Takes input for the type of operation to be performed on those integers.
 operation = input("Enter an operation: ")
 
+
+#Conditions for those operations provided their is correct input by user.
 if operation == "+":
   print(num1 + num2)
 elif operation == "-":
@@ -20,4 +25,5 @@ elif operation == "**":
 else:
   print("Invalid operation!")
 
+#Farewell message output using user's nickname.
 print(f"Thank you for using the calculator {nickName} !")
