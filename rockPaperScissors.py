b77c9a0cfd5107a24b4b034fe49b9333e2c8a9ab
Electiveclone45 <92637810+Electiveclone45@users.noreply.github.com>
2024-05-02 17:37:44 +1000

    Update rockPaperScissors.py

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,51 +1,67 @@
 print("Rock, paper, scissors game")
-print("Select your move (R, P, or S)")
+print("Select your move (R, P or S)")
 print()
+
+player1Move = input("Player 1 : ").upper()
+player2Move = input("Player 2 : ").upper()
+print(player1Move, player2Move)
 
 score1 = 0
 score2 = 0
 
 while score1 < 5 and score2 < 5:
-    player1Move = input("Player 1: ").upper()
-    player2Move = input("Player 2: ").upper()
+  player1Move = input("Player 1 : ").upper()
+  player2Move = input("Player 2 : ").upper()
 
-    if player1Move == "R":
-        if player2Move == "R":
-            print("You both picked Rock, draw!")
-        elif player2Move == "S":
-            print("Player 1 smashed Player 2's Scissors into dust with their Rock!")
-            score1 += 1
-        elif player2Move == "P":
-            print("Player 1's Rock is smothered by Player 2's Paper!")
-            score2 += 1
-        else:
-            print("Invalid Move Player 2!")
-            break
-    elif player1Move == "P":
-        if player2Move == "P":
-            print("You both picked paper, draw!")
-        elif player2Move == "S":
-            print("Player 1's paper is cut into tiny pieces by Player 2's Scissors!")
-            score2 += 1
-        elif player2Move == "R":
-            print("Player 2's Rock is smothered by Player 1's Paper!")
-            score1 += 1
-        else:
-            print("Invalid Move Player 2!")
-            break
-    elif player1Move == "S":
-        if player2Move == "R":
-            print("Player 2's Rock makes metal-dust out of Player 1's Scissors")
-        elif player2Move == "S":
-            print("Ka-Shing! Scissors bounce off each other like a dodgy sword fight!")
-        elif player2Move == "P":
-            print("Player 1's Scissors make confetti out of Player 2's paper!")
-            score1 += 1
-        else:
-            print("Invalid Move Player 2!")
-            break
+  if player1Move == "R":
+    if player2Move == "R":
+      print("You both picked Rock, draw!")
+    elif player2Move == "S":
+      print("Player1 smashed Player2's Scissors into dust with their Rock!")
+      score1 += 1
+    elif player2Move == "P":
+      print("Player1's Rock is smothered by Player2's Paper!")
+      score2 += 1
     else:
-        print("Invalid Move Player 1")
-        break
+      print("Invalid Move Player 2!")
+      break
+
+  elif player1Move == "P":
+    if player2Move == "P":
+      print("You both picked paper, draw!")
+    elif player2Move == "S":
+      print("Player1's paper is cut into tiny pieces by Player2's Scissors!")
+      score2 += 1
+    elif player2Move == "S":
+      print("Player2's Rock is smothered by Player1's Paper!")
+      score1 += 1
+    else:
+      print("Invalid Move Player 2!")
+      break
+
+  elif player1Move == "S":
+    if player2Move == "R":
+      print("Player 2's Rock makes metal-dust out of Player1's Scissors")
+    elif player2Move == "S":
+      print(
+          "Ka-Shing! Scissors bounce off each other like a dodgy sword fight!")
+      score2 += 1
+    elif player2Move == "P":
+      print("Player1's Scissors make confetti out of Player2's paper!")
+      score1 += 1
+    else:
+      print("Invalid Move Player 2!")
+      break
+  else:
+    print("Invalid Move Player 1")
+    break
+
+if score1 == 5:
+  print("Player 1 wins!")
+elif score2 == 5:
+  print("Player 2 wins!")
+
+print("Game Over!")
+
 
 print("Game Over!")
